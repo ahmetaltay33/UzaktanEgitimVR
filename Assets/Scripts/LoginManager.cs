8d3379535eeda049111a8b17d27a8bd0724972e4
ahmetaltay33 <altaybilisim@gmail.com>
2021-04-01 19:30:02 +0300

    Boş login sahnesi oluşturuldu. Photon basit bağlantı scripti yazıldı.

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        Debug.Log("OnConnected çağrıldı. Sunucu erişilebilir durumda.");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Ana sunucuya bağlantı kuruldu.");
+    }
+}
