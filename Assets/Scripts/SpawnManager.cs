daa92f4e29ff1343f778beed09744bb7ce262cab
ahmetaltay33 <altaybilisim@gmail.com>
2021-04-03 18:03:02 +0300

    SpawnManager ve VirtualWorldManager oluşturuldu.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GenericVRPlayerPrefab;
+
+    public Vector3 SpawnPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, SpawnPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VirtualWorldManager.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class VirtualWorldManager : MonoBehaviourPunCallbacks
+{
+
+    #region Photon Callbacks Methods
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log($"Oyuncu {newPlayer.NickName}, {PhotonNetwork.CurrentRoom.Name} odaya katıldı. Oyuncu sayısı: {PhotonNetwork.CurrentRoom.PlayerCount}");
+    }
+
+    #endregion
+    
+}
