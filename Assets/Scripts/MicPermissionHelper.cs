87cc1faa64a44f80bb8edd9093aedca944b2f835
ahmetaltay33 <altaybilisim@gmail.com>
2021-04-04 20:40:50 +0300

    Android için mikrofon erişim izini alma eklendi.

---
+++ Assets/Scripts/MicPermissionHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+
+#endif
+
+public class MicPermissionHelper : MonoBehaviour
+{
+    void Start()
+
+    {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+#endif
+    }
+}
