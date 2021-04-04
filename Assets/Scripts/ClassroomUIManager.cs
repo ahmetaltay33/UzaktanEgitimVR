9c8e24cdd2cce9206098108db05c777f5b5dcdf4
ahmetaltay33 <altaybilisim@gmail.com>
2021-04-04 14:29:29 +0300

    Sahnelerde MainCamera ayarları yapıldı. Classroom ve Lab dan çıkış için kapılara çıkış buttonu eklendi.

---
+++ Assets/Scripts/ClassroomUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassroomUIManager : MonoBehaviour
+{
+    public void ExitClassroom_OnClick()
+    {
+        VirtualWorldManager.Instance.LeaveRoomAndLoadHomeScene();    
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LabUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LabUIManager : MonoBehaviour
+{
+
+    public void ExitLab_OnClick()
+    {
+        SceneManager.LoadScene("Scenes/LoginScene");
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
