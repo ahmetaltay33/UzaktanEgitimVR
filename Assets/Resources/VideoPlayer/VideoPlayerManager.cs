a048bc85551fb3c0578c3244d2413b7a648983d6
ahmetaltay33 <altaybilisim@gmail.com>
2021-06-06 16:41:14 +0300

    Video ilerlemesinin gözlenebilmesi için VideoPlayerManager oluşturuldu.

---
+++ Assets/Resources/VideoPlayer/VideoPlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private Image imageProgress;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (videoPlayer.frameCount > 0)
+            imageProgress.fillAmount = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
+    }
+}
